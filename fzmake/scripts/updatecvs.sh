e1e00c4d320c6ff5f3e176874c5e10322181909b
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2007-01-08 15:22:50 +0000

    Fix cvs update

--- fzmake/scripts/updatecvs.sh
+++ fzmake/scripts/updatecvs.sh
@@ -5,10 +5,11 @@ function updatecvs()
   logprint "Updating CVS"
   logprint "------------"
   
-  for i in $PACKAGES; do
-    i=${i#-}
-    logprint "Updating package $i"
-    cd $CVSDIR/$i
+  resetPackages
+  while getPackage; do
+    PACKAGE=${PACKAGE#-}
+    logprint "Updating package $PACKAGE"
+    cd $CVSDIR/$PACKAGE
     cvs -q -z3 update -dP >> $LOG 2>&1 || return 1
   done
 }
