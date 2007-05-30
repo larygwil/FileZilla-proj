0db9636407c0bb1a89541a6a0d043dab792af6a0
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2007-05-30 14:45:19 +0000

    Use $WWWDIR

--- fzmake/scripts/clean.sh
+++ fzmake/scripts/clean.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
-NIGHTLYDIR=/var/www/nightlies
 DAYS=3		# Minumum number of days to keep nightlies from
 
-DATES=`ls $NIGHTLYDIR | grep "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" | sort -r`
+DATES=`ls "$WWWDIR" | grep "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" | sort -r`
 
 # List of targets which were compiled successfully
 TARGETS=""
@@ -34,12 +33,12 @@ for i in $DATES; do
     DAYS=`expr $DAYS - 1`
   fi
 
-  cd "$NIGHTLYDIR/$i"
+  cd "$WWWDIR/$i"
   for TARGET in *; do
     if ! [ -f "$TARGET/build.log" ]; then
       continue;
     fi
-    if [ -e "$NIGHTLYDIR/$i/$TARGET/successful" ]; then
+    if [ -e "$WWWDIR/$i/$TARGET/successful" ]; then
       if ! hasTarget $TARGET; then
         if [ "$DELETE" != "0" ]; then
 	  echo "Keeping $i for $TARGET";
@@ -52,7 +51,7 @@ for i in $DATES; do
 
   if [ "$DELETE" = "1" ]; then
     echo "Deleting nightly $i"
-    rm -rf "$NIGHTLYDIR/$i"
+    rm -rf "$WWWDIR/$i"
   fi
 
 done
