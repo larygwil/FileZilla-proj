0451f344af0f2a8fca846101f33c0ebbfcffe55c
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2007-05-30 14:51:15 +0000

    Modularize postbuild scripts

---
+++ fzmake/scripts/postbuild.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+postbuild()
+{
+  load_file "$CONFDIR/postbuild" || return 1
+
+  while read_file; do
+    
+    if [ -z "$REPLY" ]; then continue; fi
+
+    local script="$SCRIPTS/postbuildscripts/$REPLY"
+
+    if ! [ -x "$script" ]; then
+      echo "$script not found or not executable"
+      continue
+    fi
+
+    . "$script"
+
+  done
+}
+
---
+++ fzmake/scripts/readfile.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+load_file()
+{
+  if ! exec 3<$1; then
+    echo "Failed to open '$1'"
+    return 1
+  fi
+
+  return 0
+}
+
+read_file()
+{
+  while read -u3; do
+    if [ "${REPLY:0:1}" = "#" ]; then
+      continue
+    fi
+
+    if ! [ -z "$1" ]; then
+      export $1=$REPLY
+    fi
+
+    return 0
+  done
+
+  return 1
+}
+
