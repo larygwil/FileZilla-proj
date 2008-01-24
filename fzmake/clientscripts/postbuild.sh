dcda893584e6f3cd1943d02fb05b67884032ecf0
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2008-01-24 11:58:46 +0000

    Use Unicode NSIS through wine. Strip executables on OS X

--- fzmake/clientscripts/postbuild.sh
+++ fzmake/clientscripts/postbuild.sh
@@ -17,8 +17,21 @@ if echo "$TARGET" | grep "mingw"; then
 
   # We don't need debug information for this file. Since it runs in the context of Explorer, it's pretty undebuggable anyhow.
   strip ../src/fzshellext/.libs/libfzshellext-0.dll
-  
-  makensis install.nsi
+
+  # Convert slashes into backslashes or makensis will complain
+  cat install.nsi | sed '/define top_srcdir/s/\//\\/g' > install.nsi2
+  cat install.nsi2 | sed '/define srcdir/s/\//\\/g' > install.nsi
+
+  # Prepare files for Unicode NSIS
+  # See http://www.scratchpaper.com/ for details
+  cat install.nsi | sed 's/${top_srcdir}\\COPYING/COPYING/' > install.nsi2
+  cat install.nsi2 | iconv -f utf8 -t utf16 > install.nsi
+  rm install.nsi2
+  cat "$PREFIX/packages/FileZilla3/COPYING" | iconv -f utf8 -t utf16 > COPYING
+
+  # makensis install.nsi
+  wine /home/nightlybuild/NSIS_unicode/makensis.exe install.nsi
+
   chmod 775 FileZilla_3_setup.exe
   mv FileZilla_3_setup.exe "$OUTPUTDIR/$TARGET"
 
@@ -27,6 +40,8 @@ if echo "$TARGET" | grep "mingw"; then
   
 elif [ \( "$TARGET" = "i686-apple-darwin8" -o "$TARGET" = "powerpc-apple-darwin8" \) -a "$PACKAGE" = "FileZilla3" ]; then
   cd "$WORKDIR/$PACKAGE"
+  strip FileZilla.app/Contents/MacOS/filezilla
+  strip FileZilla.app/Contents/MacOS/fzsftp
   tar -cjf "$OUTPUTDIR/$TARGET/$PACKAGE.app.tar.bz2" FileZilla.app
 else
   cd "$WORKDIR/prefix"
