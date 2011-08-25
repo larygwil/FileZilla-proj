b7d680bc4ba6c6504cc15537d9e18429021ac647
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2011-08-25 06:46:54 +0000

    Missing commit

--- chess/calc.hpp
+++ chess/calc.hpp
@@ -5,7 +5,18 @@
 #include "detect_check.hpp"
 #include "pvlist.hpp"
 
-bool calc( position& p, color::type c, move& m, int& res, int time_limit, int clock );
+
+class seen_positions {
+public:
+	unsigned long long pos[150]; // Must be at least 50 full moves + max depth
+	int root_position; // Index of first move after root position in seen_positions
+
+	bool is_two_fold( unsigned long long hash, int depth ) const;
+	bool is_three_fold( unsigned long long hash, int depth ) const;
+};
+
+// May modify seen_positions at indexes > root_position
+bool calc( position& p, color::type c, move& m, int& res, int time_limit, int clock, seen_positions& seen );
 
 struct context
 {
@@ -13,6 +24,8 @@ struct context
 	int quiescence_depth;
 	unsigned char clock; // The halfmove clock
 	pv_entry_pool pv_pool;
+
+	seen_positions seen;
 };
 
 short step( int depth, context& ctx, position const& p, unsigned long long hash, int current_evaluation, color::type c, short alpha, short beta, pv_entry* pv );
