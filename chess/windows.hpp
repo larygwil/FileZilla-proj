92a07ab9d36d109d692c8e0ee0047f9764e11d12
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2012-03-02 06:52:48 +0000

    Support for old CPUs without native popcount. Based on patch by Jim Ablett.

--- chess/windows.hpp
+++ chess/windows.hpp
@@ -1,6 +1,8 @@
 #ifndef __WINDOWS_H__
 #define __WINDOWS_H__
 
+#define HAS_NATIVE_POPCOUNT 1
+
 #include <windows.h>
 #include <intrin.h>
 
@@ -104,7 +106,21 @@ int get_cpu_count();
 // In MiB
 int get_system_memory();
 
+#if HAS_NATIVE_POPCOUNT
+
 #define popcount __popcnt64
+
+#else
+
+inline unsigned long long popcount( unsigned long long w )
+{
+      w = w - ((w >> 1) & 0x5555555555555555ull);
+      w = (w & 0x3333333333333333ull) + ((w >> 2) & 0x3333333333333333ull);
+      w = (w + (w >> 4)) & 0x0f0f0f0f0f0f0f0full;
+      return (w * 0x0101010101010101ull) >> 56;
+}
+
+#endif
 
 #define atoll _atoi64
 
