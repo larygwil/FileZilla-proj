d07ea3003aea058947d720806cedec33d20dccc5
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2012-03-31 07:41:45 +0000

    Add to_string function.

--- chess/string.hpp
+++ chess/string.hpp
@@ -3,7 +3,18 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 
 std::string split( std::string const& str, std::string& args, std::string::value_type c = ' ' );
+
+
+template< typename T >
+inline std::string to_string( T const& t )
+{
+	std::stringstream ss;
+	ss << t;
+	return ss.str();
+}
+
 
 #endif
