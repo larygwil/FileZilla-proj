beddcdfd64f8a1701e88a93b48d159126bc58b43
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2012-04-06 17:34:51 +0000

    Fix stupid see bug: Checking that an attacker exist didn't consider side-specific pieces but all pieces instead. Also reworked the loop such that the attacker check is done earlier, this speeds up see nicely.

--- chess/see.cpp
+++ chess/see.cpp
@@ -9,26 +9,27 @@
 #include <algorithm>
 #include <iostream>
 
-static uint64_t least_valuable_attacker( position const& p, color::type c, uint64_t attackers, pieces::type& attacker_piece_out ) {
-	// Exploit that our bitboards are sorted by piece value
-	for( int piece = pieces::pawn; piece <= pieces::king; ++piece ) {
-		uint64_t match = p.bitboards[c].b[piece] & attackers;
-		if( match ) {
-			attacker_piece_out = static_cast<pieces::type>(piece);
-			uint64_t ret = bitscan( match );
-			return 1ull << ret;
-		}
+static uint64_t least_valuable_attacker( position const& p, color::type c, uint64_t attackers, pieces::type& attacker_piece_out )
+{
+	// Exploit that our bitboards are sorted by piece value and that we do have an attacker.
+	uint64_t match;
+
+	int piece = pieces::pawn;
+	while( !(match = p.bitboards[c].b[piece] & attackers) ) {
+		++piece;
 	}
 
-	return 0;
+	attacker_piece_out = static_cast<pieces::type>(piece);
+	uint64_t ret = bitscan( match );
+	return 1ull << ret;
 }
+
 
 int see( position const& p, color::type c, move const& m )
 {
 	// Iterative SEE algorithm as described by Fritz Reul, adapted to use bitboards.
 	unsigned char target = m.target;
 
-	uint64_t attacker_mask = 1ull << m.source;
 	pieces::type attacker_piece = m.piece;
 
 	int score[32];
@@ -37,6 +38,7 @@ int see( position const& p, color::type c, move const& m )
 	uint64_t const all_bishops = p.bitboards[color::white].b[bb_type::bishops] | p.bitboards[color::white].b[bb_type::queens] | p.bitboards[color::black].b[bb_type::bishops] | p.bitboards[color::black].b[bb_type::queens];
 
 	uint64_t all_pieces = p.bitboards[color::white].b[bb_type::all_pieces] | p.bitboards[color::black].b[bb_type::all_pieces];
+	all_pieces ^= 1ull << m.source;
 
 	uint64_t attackers =
 			(rook_magic( target, all_pieces ) & all_rooks) |
@@ -45,35 +47,41 @@ int see( position const& p, color::type c, move const& m )
 			(possible_king_moves[target] & (p.bitboards[color::white].b[bb_type::king] | p.bitboards[color::black].b[bb_type::king])) |
 			(pawn_control[color::black][target] & p.bitboards[color::white].b[bb_type::pawns]) |
 			(pawn_control[color::white][target] & p.bitboards[color::black].b[bb_type::pawns]);
-
-	int depth = 0;
+	// Don't have to remove source piece here, done implicitly in the loop.
 
 	score[0] = eval_values::mg_material_values[ m.captured_piece ];
 
+	// Get new attacker
+	if( !(attackers & p.bitboards[1-c].b[bb_type::all_pieces]) ) {
+		return score[0];
+	}
+
+	int depth = 1;
+
 	// Can "do", as we always have at least one.
 	do {
-		++depth;
 		score[depth] = eval_values::mg_material_values[ attacker_piece ] - score[depth - 1];
 
 		if( score[depth] < 0 && score[depth - 1] < 0 ) {
 			break; // Bad capture
 		}
 
+		// We have verified that there's already at least one attacker, so this works.
+		uint64_t attacker_mask = least_valuable_attacker( p, static_cast<color::type>(c ^ (depth & 1)), attackers, attacker_piece );
+
+		++depth;
+
 		// Remove old attacker
 		all_pieces ^= attacker_mask;
-		attackers ^= attacker_mask;
 
 		// Update attacker list due to uncovered attacks
-		// Todo: Only update specific ray
-		attackers |= ((all_pieces & rook_magic( target, all_pieces )) & all_rooks) |
-						((all_pieces & bishop_magic( target, all_pieces )) & all_bishops);
+		attackers |= ((rook_magic( target, all_pieces )) & all_rooks) |
+						((bishop_magic( target, all_pieces )) & all_bishops);
+		attackers &= all_pieces;
 
-		if( !attackers ) {
+		if( !(attackers & p.bitboards[c ^ (depth & 1)].b[bb_type::all_pieces]) ) {
 			break;
 		}
-
-		// Get new attacker
-		attacker_mask = least_valuable_attacker( p, static_cast<color::type>(c ^ (depth & 1)), attackers, attacker_piece );
 	} while( true );
 
 	// Propagate scores back
