c7da04d6cbf8ad450b070f9c20c4d6ed3e99ec68
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2012-06-11 16:06:45 +0000

    Fix some warnings.

--- chess/calc.hpp
+++ chess/calc.hpp
@@ -22,7 +22,7 @@ struct def_new_best_move_callback : public new_best_move_callback_base
 
 struct null_new_best_move_callback : public new_best_move_callback_base
 {
-	virtual void on_new_best_move( position const& p, color::type c, int depth, int selective_depth, int evaluation, uint64_t nodes, duration const& elapsed, pv_entry const* pv ) {}
+	virtual void on_new_best_move( position const&, color::type, int, int, int, uint64_t, duration const&, pv_entry const* ) {}
 };
 
 extern def_new_best_move_callback default_new_best_move_callback;
