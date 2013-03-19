c4b3c4fe17dea72fa5faf00805a680b2453dc832
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2013-03-19 22:01:45 +0000

    Incorporate new book into relase build script

--- chess/workspace/build.sh
+++ chess/workspace/build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+MAKEOPTS=-j4
+
 if [ -z "$REV" ]; then
   REV=`svn info . | grep '^Revision: ' | grep -o '[0-9]\+'`
 fi
@@ -15,19 +17,20 @@ rm -rf octochess-r$REV
 mkdir -p octochess-r$REV
 
 make clean
-make -f Makefile-opt chess-use REVISION="-DREVISION=\\\"$REV\\\""
+make ${MAKEOPTS} bookgen
+./bookgen fold
+./bookgen "export octochess-r$REV/octochess.book"
+
+make clean
+make ${MAKEOPTS} -f Makefile-opt chess-use REVISION="-DREVISION=\\\"$REV\\\""
 cp chess-use octochess-r$REV/octochess-linux-sse4-r$REV
 
 rm -f chess-use
 make clean
-make -f Makefile-opt ARCH=core2 chess-use REVISION="-DREVISION=\\\"$REV\\\"" EXTRA_CPPFLAGS="-DUSE_GENERIC_POPCOUNT=1"
+make ${MAKEOPTS} -f Makefile-opt ARCH=core2 chess-use REVISION="-DREVISION=\\\"$REV\\\"" EXTRA_CPPFLAGS="-DUSE_GENERIC_POPCOUNT=1"
 cp chess-use octochess-r$REV/octochess-linux-generic-r$REV
 
 strip octochess-r$REV/*
-
-cp opening_book.db octochess-r$REV/
-sqlite3 octochess-r$REV/opening_book.db 'delete from position where data IS NULL;'
-sqlite3 octochess-r$REV/opening_book.db 'vacuum full'
 
 cp AUTHORS octochess-r$REV/authors.txt
 cp COPYING octochess-r$REV/copying.txt
