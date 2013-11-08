58ddf2f17d7fa3bf5e314b4ca48b8c47d399f5d0
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2013-11-08 19:50:53 +0000

    Avoid a copy

--- chess/pv_move_picker.cpp
+++ chess/pv_move_picker.cpp
@@ -12,7 +12,7 @@ std::pair<move,move> pv_move_picker::can_use_move_from_pv( position const& p )
 {
 	std::pair<move,move> ret;
 
-	if( p.hash_ == hash_ ) {
+	if( p.hash_ == next_pos_.hash_ ) {
 
 		// Recapture
 		if( previous_pos_.get_captured_piece(previous_) != pieces::none && next_pos_.get_captured_piece(next_) != pieces::none && previous_.target() == next_.target() ) {
@@ -25,15 +25,15 @@ std::pair<move,move> pv_move_picker::can_use_move_from_pv( position const& p )
 }
 
 
-void pv_move_picker::update_pv( position p, move const* pv )
+void pv_move_picker::update_pv( position const& p, move const* pv )
 {
 	if( pv && !pv->empty() && !pv[1].empty() && !pv[2].empty() ) {
-		apply_move( p, *pv );
 		previous_pos_ = p;
-		apply_move( p, pv[1] );
-		next_pos_ = p;
-
-		hash_ = p.hash_;
+		apply_move( previous_pos_, *pv );
+		next_pos_ = previous_pos_;
+		apply_move( next_pos_, pv[1] );
+		
+		hash_ = next_pos_.hash_;
 		previous_ = pv[1];
 		next_ = pv[2];
 
