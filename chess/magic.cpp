74ee6506b8feeb9db788e38dcd8941f4f3a8540d
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2014-01-12 09:41:53 +0000

    Simplify code, no functional change.

--- chess/magic.cpp
+++ chess/magic.cpp
@@ -27,65 +27,43 @@ uint64_t expand( uint64_t value, uint64_t mask )
 
 	return ret;
 }
-}
 
+uint64_t* init_magic( uint64_t* offsets, uint64_t const* shifts, uint64_t const* masks, uint64_t const* multipliers, uint64_t (*attacks)(uint64_t, uint64_t) ) {
+	uint64_t offset = 0;
+	
+	for( unsigned int pi = 0; pi < 64; ++pi ) {
+		offsets[pi] = offset;
+		offset += 1ull << (64 - shifts[pi]);
+	}
+
+	uint64_t * values = new uint64_t[offset];
+	memset( values, 0, sizeof(uint64_t) * offset );
+
+	offset = 0;
+	for( unsigned int pi = 0; pi < 64; ++pi ) {
+			
+		for( unsigned int i = 0; i < (1ull << (64 - shifts[pi])); ++i ) {
+			uint64_t occ = expand( i, masks[pi] );
+
+			uint64_t key = occ * multipliers[pi];
+			key >>= shifts[pi];
+
+			uint64_t const attack = attacks( pi, occ );
+			ASSERT( !values[offset + key] || values[offset + key] == attack );
+			values[offset + key] = attack;
+		}
+			
+		offset += 1ull << (64 - shifts[pi]);
+	}
+
+	return values;
+}
+}
 
 void init_magic()
 {
-	{
-		uint64_t offset = 0;
-		for( unsigned int pi = 0; pi < 64; ++pi ) {
-			rook_magic_offsets[pi] = offset;
-			offset += 1ull << (64 - rook_magic_shift[pi]);
-		}
-
-		rook_magic_values = new uint64_t[offset];
-		memset( rook_magic_values, 0, sizeof(uint64_t) * offset );
-
-		offset = 0;
-		for( unsigned int pi = 0; pi < 64; ++pi ) {
-			
-			for( unsigned int i = 0; i < (1ull << (64 - rook_magic_shift[pi])); ++i ) {
-				uint64_t occ = expand( i, rook_magic_mask[pi] );
-
-				uint64_t key = occ * rook_magic_multiplier[pi];
-				key >>= rook_magic_shift[pi];
-
-				uint64_t const attack = rook_attacks( pi, occ );
-				ASSERT( !rook_magic_values[offset + key] || rook_magic_values[offset + key] == attack );
-				rook_magic_values[offset + key] = attack;
-			}
-			
-			offset += 1ull << (64 - rook_magic_shift[pi]);
-		}
-	}
-	{
-		uint64_t offset = 0;
-		for( unsigned int pi = 0; pi < 64; ++pi ) {
-			bishop_magic_offsets[pi] = offset;
-			offset += 1ull << (64 - bishop_magic_shift[pi]);
-		}
-
-		bishop_magic_values = new uint64_t[offset];
-		memset( bishop_magic_values, 0, sizeof(uint64_t) * offset );
-
-		offset = 0;
-		for( unsigned int pi = 0; pi < 64; ++pi ) {
-			
-			for( unsigned int i = 0; i < (1ull << (64 - bishop_magic_shift[pi])); ++i ) {
-				uint64_t occ = expand( i, bishop_magic_mask[pi] );
-
-				uint64_t key = occ * bishop_magic_multiplier[pi];
-				key >>= bishop_magic_shift[pi];
-
-				uint64_t const attack = bishop_attacks( pi, occ );
-				ASSERT( !bishop_magic_values[offset + key] || bishop_magic_values[offset + key] == attack );
-				bishop_magic_values[offset + key] = attack;
-			}
-			
-			offset += 1ull << (64 - bishop_magic_shift[pi]);
-		}
-	}
+	rook_magic_values = init_magic( rook_magic_offsets, rook_magic_shift, rook_magic_mask, rook_magic_multiplier, rook_attacks );
+	bishop_magic_values = init_magic( bishop_magic_offsets, bishop_magic_shift, bishop_magic_mask, bishop_magic_multiplier, bishop_attacks );
 }
 
 uint64_t rook_magic( uint64_t pi, uint64_t occ )
