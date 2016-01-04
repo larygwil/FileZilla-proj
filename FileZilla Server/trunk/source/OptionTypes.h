9cfc8c2d442cf665d507ead4823db16fc2bf199f
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2016-01-04 14:00:32 +0000

    Forgot to commit the 0.9.54 version bump.

--- FileZilla Server/trunk/source/OptionTypes.h
+++ FileZilla Server/trunk/source/OptionTypes.h
@@ -90,8 +90,8 @@ struct t_Option
 	BOOL bOnlyLocal; //If TRUE, setting can only be changed from local connections
 };
 
-const DWORD SERVER_VERSION = 0x00095300;
-const DWORD PROTOCOL_VERSION = 0x00012000;
+const DWORD SERVER_VERSION = 0x00095400;
+const DWORD PROTOCOL_VERSION = 0x00013000;
 
 //												Name					Type		Not remotely
 //																(0=str, 1=numeric)   changeable
