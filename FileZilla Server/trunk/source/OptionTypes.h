1d84574e332c93d3c4e2362032707fec1fcee2f3
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2016-09-22 14:04:45 +0000

    Bump to FileZilla Server 0.9.59

--- FileZilla Server/trunk/source/OptionTypes.h
+++ FileZilla Server/trunk/source/OptionTypes.h
@@ -90,7 +90,7 @@ struct t_Option
 	BOOL bOnlyLocal; //If TRUE, setting can only be changed from local connections
 };
 
-const DWORD SERVER_VERSION = 0x00095800;
+const DWORD SERVER_VERSION = 0x00095900;
 const DWORD PROTOCOL_VERSION = 0x00014000;
 
 //												Name					Type		Not remotely
