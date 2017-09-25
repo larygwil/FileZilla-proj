3ad5029630034dbdb0b211791e821a0f45e13384
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2017-09-25 14:11:00 +0000

    Add fz::buffer

---
+++ libfilezilla/trunk/lib/buffer.cpp
@@ -0,0 +1,145 @@
+#include "libfilezilla/buffer.hpp"
+
+#include <algorithm>
+
+namespace fz {
+
+buffer::buffer(size_t capacity)
+{
+	reserve(capacity);
+}
+
+buffer::buffer(buffer const& buf)
+{
+	if (buf.size_) {
+		data_ = new unsigned char[buf.capacity_];
+		memcpy(data_, buf.pos_, buf.size_);
+size_ = buf.size_;
+capacity_ = buf.capacity_;
+pos_ = data_;
+	}
+}
+
+buffer::buffer(buffer && buf)
+{
+	data_ = buf.data_;
+	buf.data_ = nullptr;
+	pos_ = buf.pos_;
+	buf.pos_ = nullptr;
+	size_ = buf.size_;
+	capacity_ = buf.capacity_;
+}
+
+unsigned char* buffer::get(size_t write_size)
+{
+	if (capacity_ - (pos_ - data_) - size_ < write_size) {
+		if (capacity_ - size_ > write_size) {
+			memmove(data_, pos_, size_);
+			pos_ = data_;
+		}
+		else {
+			capacity_ = std::max({ 1024u, capacity_ * 2, capacity_ + write_size });
+			unsigned char* data = new unsigned char[capacity_];
+			if (size_) {
+				memcpy(data, pos_, size_);
+			}
+			else {
+				delete[] data_;
+			}
+			data_ = data;
+			pos_ = data;
+		}
+	}
+	return data_;
+}
+
+buffer& buffer::operator=(buffer const& buf)
+{
+	delete[] data_;
+	if (buf.size_) {
+		data_ = new unsigned char[buf.capacity_];
+		memcpy(data_, buf.pos_, buf.size_);
+	}
+	else {
+		data_ = nullptr;
+	}
+	size_ = buf.size_;
+	capacity_ = buf.capacity_;
+	pos_ = data_;
+
+	return *this;
+}
+
+buffer& buffer::operator=(buffer && buf)
+{
+	delete[] data_;
+	data_ = buf.data_;
+	buf.data_ = nullptr;
+	pos_ = buf.pos_;
+	buf.pos_ = nullptr;
+	size_ = buf.size_;
+	capacity_ = buf.capacity_;
+	return *this;
+}
+
+
+void buffer::add(size_t added)
+{
+	if (capacity_ - (pos_ - data_) - size_ < added) {
+		// Hang, draw and quarter the caller.
+		abort();
+	}
+	size_ += added;
+}
+
+void buffer::consume(size_t consumed)
+{
+	if (consumed > size_) {
+		abort();
+	}
+	if (consumed == size_) {
+		pos_ = data_;
+		size_ = 0;
+	}
+	else {
+		size_ -= consumed;
+		pos_ += consumed;
+	}
+}
+
+void buffer::clear()
+{
+	size_ = 0;
+	pos_ = data_;
+}
+
+void buffer::append(unsigned char const* data, size_t len)
+{
+	memcpy(get(len), data, len);
+	size_ += len;
+}
+
+void buffer::append(std::string const& str)
+{
+	append(reinterpret_cast<unsigned char const*>(str.c_str()), str.size());
+}
+
+void buffer::reserve(size_t capacity)
+{
+	if (capacity_ > capacity) {
+		return;
+	}
+
+	capacity_ = std::max(1024u, capacity);
+	unsigned char* data = new unsigned char[capacity_];
+	if (size_) {
+		memcpy(data, pos_, size_);
+	}
+	else {
+		delete[] data_;
+	}
+	data_ = data;
+	pos_ = data_;
+}
+
+}
---
+++ libfilezilla/trunk/lib/libfilezilla/buffer.hpp
@@ -0,0 +1,89 @@
+#ifndef LIBFILEZILLA_BUFFER_HEADER
+#define LIBFILEZILLA_BUFFER_HEADER
+
+#include "libfilezilla.hpp"
+
+/** \file
+* \brief Declares fz::buffer
+*/
+
+namespace fz {
+
+/**
+ * \brief The buffer class is a simple buffer where data can be appended at the end and consumed at the front.
+ * Think of it as a deque with contiguous storage.
+ *
+ * This class is useful when buffering data for sending over the network, or for buffering data for further 
+ * piecemeal processing after having received it.
+ */
+class FZ_PUBLIC_SYMBOL buffer final
+{
+public:
+	buffer() = default;
+
+	/// Initially reserves the passed capacity
+	explicit buffer(size_t capacity);
+
+	buffer(buffer const& buf);
+	buffer(buffer && buf);
+
+	~buffer() { delete data_; }
+
+	buffer& operator=(buffer const& buf);
+	buffer& operator=(buffer && buf);
+
+	// Undefined if buffer is empty
+	unsigned char* get() { return pos_; }
+
+	/** \brief Returns a writable buffer guaranteed to be large enough for write_size bytes, call add when done.
+	 *
+	 * \sa append
+	 */
+	unsigned char* get(size_t write_size);
+
+	// Increase size by the passed amount. Call this after having obtained a writable buffer with get(size_t write_size)
+	void add(size_t added);
+
+	/** \brief Removes consumed bytes from the beginning of the buffer.
+	 *
+	 * Undefined if consumed > size
+	 */
+	void consume(size_t consumed);
+
+	size_t size() const { return size_; }
+
+	void clear();
+
+	/** \brief Appends the passed data to the buffer.
+	 *
+	 * The number of reallocations as result to repeated append are armortized O(1)
+	 */
+	void append(unsigned char const* data, size_t len);
+	void append(std::string const& str);
+
+	bool empty() const { return size_ == 0; }
+	explicit operator bool() const {
+		return size_ != 0;
+	}
+
+	void reserve(size_t capacity);
+
+	/// Gets element at offset i. Does not do bounds checking
+	unsigned char operator[](size_t i) const { return pos_[i]; }
+	unsigned char & operator[](size_t i) { return pos_[i]; }
+private:
+
+	// Invariants:
+	//   size_ <= capacity_
+	//   data_ <= pos_
+	//   pos_ <= data_ + capacity_
+	//   pos_ + size_ <= data_ + capacity_
+	unsigned char* data_{};
+	unsigned char* pos_{};
+	size_t size_{};
+	size_t capacity_{};
+};
+
+}
+
+#endif
